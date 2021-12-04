587b842d2f217b7977473d5bdb90c87697832924
John Hur <hur.john628@gmail.com>
2021-12-04 01:15:14 -0600

    added addMenu function

--- index.js
+++ index.js
@@ -11,8 +11,92 @@ const inquirer = require("inquirer");
 let team = [];
 
 // Function to prompt user to add more roles
-const addMenu() => {
-    
+const addMenu = () => {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "newEmployee",
+            message: "Add a new employee?",
+            choices: [
+                "Engineer",
+                "Intern",
+                "Done with employee list"
+            ]
+        }
+    ]).then(data => {
+        switch (data.employeeType) {
+          case "Engineer":
+            engineerPrompt();
+          break;
+          case "Intern":
+            internPrompt();
+          break;
+          case "Done with employee list":
+            renderCards(); // placeholder for function I will make
+          break;
+        default:
+          console.log("Choose an employee you want to add!");
+        }
+})}
+
+// Function that have questions to prompt for intern
+const internPrompt = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What's your intern's name?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is the intern's ID?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is the intern's email address?"
+        },
+        {
+            type: "input",
+            name: "school",
+            message: "What is your intern's school?"
+        }
+    ]).then(data => {
+        const intern = new Intern(data.name, data.id, data.email, data.school);
+        team.push(intern);
+        addMenu();
+    })
+}
+
+// Function that have questions to prompt for engineer
+const engineerPrompt = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What's your engineer's name?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is the engineer's ID?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is the engineer's email address?"
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "What is your engineer's github user name?"
+        }
+    ]).then(data => {
+        const engineer = new Engineer(data.name, data.id, data.email, data.github);
+        team.push(engineer);
+        addMenu();
+    })
 }
 
 // Function that have questions to prompt for manager
