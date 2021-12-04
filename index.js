a515a1b26d9ddae3b8a9077158e0bdd8ed485dfb
John Hur <hur.john628@gmail.com>
2021-12-04 00:06:10 -0600

    added manager prompt function

--- index.js
+++ index.js
@@ -1,8 +1,49 @@
+// Add imports
+const Manager = require("./lib/manager");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
+
 // Add npm
 const fs = require("fs");
 const inquirer = require("inquirer");
 
-// Add functions that have questions to prompt for manager
-const managerPrompt = () => {
+// Make an object for the team
+let team = [];
+
+// Function to prompt user to add more roles
+const addMenu() => {
     
 }
+
+// Function that have questions to prompt for manager
+const managerPrompt = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Who is your team's manager?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is the manager's ID?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is the manager's email address?"
+        },
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "What is your manager's office number?"
+        }
+    ]).then(data => {
+        const manager = new Manager(data.name, data.id, data.email, data.officeNumber);
+        team.push(manager);
+        addMenu();
+    })
+}
+
+// Start app with managerPrompt function
+managerPrompt();
