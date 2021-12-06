db94d1a360ca94ba07dcbdc93f9e2ae6e770bf9a
John Hur <hur.john628@gmail.com>
2021-12-05 20:05:12 -0600

    added more styling

--- src/renderHTML.js
+++ src/renderHTML.js
@@ -82,6 +82,9 @@ const generateHTML = (cards) => {
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         </head>
         <body>
+            <header class="jumbotron jumbotron-fluid bg-dark text-light">
+                <div class="container">Your Team</div>
+            </header>
             <div class="row justify-content-center">
                 ${cards}
             </div>
