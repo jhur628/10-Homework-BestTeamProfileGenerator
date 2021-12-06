8bb8fb34cff678b2f792ab6017311e2a25883022
John Hur <hur.john628@gmail.com>
2021-12-05 21:10:15 -0600

    added test for manager.js

--- tests/manager.test.js
+++ tests/manager.test.js
@@ -1,9 +1,13 @@
 const Manager = require("../lib/manager")
 
-const testEmployee = new Employee("John", 989, "john@john.com","9998887777");
+const testEmployee = new Manager("John", 989, "john@john.com", 9998887777);
 
 describe('Manager', () => {
-    it('should ', () => {
-        
+    it('should return officeNumber as 9998887777', () => {
+        expect(testEmployee.officeNumber).toBe(9998887777)
+    });
+
+    it('should return role as Manager', () => {
+        expect(testEmployee.getRole()).toBe("Manager")
     });
 });
